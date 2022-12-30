4167a9cf3d39799d7ce6cfebd335092f04822a98
MykytaX <Mykyta X>
2022-12-31 00:49:19 +0800

    New day

---
+++ 2022/Day 24/day24.py
@@ -0,0 +1,14 @@
+#E######
+#>>.<^<#
+#.<..<<#
+#>v.><>#
+#<^v^^>#
+######.#
+
+f = open("./data.txt")
+valley = []
+lines = f.readlines()
+for line in lines:
+    valley.append(line.rstrip())
+
+startpos = 
